43a0fbf0d9beece36d563af05985d08023475499
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-10-29 11:17:56 +0100

    Add project files.

---
+++ CSF_Plugin.cs
@@ -0,0 +1,49 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using EFT.InventoryLogic;
+using EFT.UI;
+using QuickSellFlea.Patches;
+
+namespace QuickSellFlea;
+
+[BepInPlugin("com.lacyway.csf", "QuickSellFlea", PluginVersion)]
+internal class CSF_Plugin : BaseUnityPlugin
+{
+    public const string PluginVersion = "1.0.0";
+
+    internal static ManualLogSource CSF_Logger;
+
+    public static ConfigEntry<bool> ShowListingPrice { get; set; }
+
+    protected void Awake()
+    {
+        CSF_Logger = Logger;
+        CSF_Logger.LogInfo($"{nameof(CSF_Plugin)} has been loaded.");
+
+        ShowListingPrice = Config.Bind("QuickSellFlea", "Show Listing Price", true,
+            new ConfigDescription("Whether to show the listing price in the tooltip, otherwise the total sell value"));
+
+        new ItemUiContext_GetItemContextInteractions_Patch().Enable();
+    }
+}
+
+public struct PostPriceData(RagFairClass ragFair, ContextInteractionsAbstractClass interactionsClass,
+        ItemUiContext itemUiContext, ItemInfoInteractionsAbstractClass<EItemInfoButton> infoInteractionsClass,
+        Dictionary<EItemInfoButton, string> itemInfoDict, InventoryController inventoryController,
+        ItemContextAbstractClass itemContext, HandbookClass handbookClass)
+{
+    public readonly Item Item => ItemContext.Item;
+
+    public readonly RagFairClass RagFair = ragFair;
+    public readonly ContextInteractionsAbstractClass InteractionsClass = interactionsClass;
+    public readonly ItemUiContext ItemUiContext = itemUiContext;
+    public readonly ItemInfoInteractionsAbstractClass<EItemInfoButton> InfoInteractionsClass = infoInteractionsClass;
+    public readonly Dictionary<EItemInfoButton, string> ItemInfoDict = itemInfoDict;
+    public readonly InventoryController InventoryController = inventoryController;
+    public readonly ItemContextAbstractClass ItemContext = itemContext;
+    public readonly HandbookClass HandbookClass = handbookClass;
+    public readonly Dictionary<Item, ItemAddress> OfferDict = [];
+
+    public float AveragePrice;
+}
---
+++ Patches/ItemUiContext_GetItemContextInteractions_Patch.cs
@@ -0,0 +1,219 @@
+﻿using Comfort.Common;
+using EFT.InventoryLogic;
+using EFT.UI;
+using EFT.UI.Ragfair;
+using SPT.Reflection.Patching;
+using System.Reflection;
+using UnityEngine;
+
+namespace QuickSellFlea.Patches;
+
+internal class ItemUiContext_GetItemContextInteractions_Patch : ModulePatch
+{
+    private static PostPriceData _postPriceData;
+    private const string _roubleTpl = "5449016a4bdc2d6f028b456f";
+    private static bool _canPost = true;
+
+
+    protected override MethodBase GetTargetMethod()
+    {
+        return typeof(ItemUiContext)
+            .GetMethod(nameof(ItemUiContext.ShowContextMenu));
+    }
+
+    [PatchPostfix]
+    public static void Postfix(ItemUiContext __instance,
+        InventoryController ___inventoryController_0, Dictionary<EItemInfoButton, string> ___dictionary_0,
+        ItemContextAbstractClass itemContext, ItemInfoInteractionsAbstractClass<EItemInfoButton> ___gclass3753_0)
+    {
+        if (!_canPost)
+        {
+            return;
+        }
+
+#pragma warning disable CS0618 // Type or member is obsolete
+        if (GClass2340.InRaid)
+        {
+            return;
+        }
+#pragma warning restore CS0618 // Type or member is obsolete
+
+        if (___gclass3753_0 == null)
+        {
+            return;
+        }
+
+#if DEBUG
+        Logger.LogInfo("Patch running");
+#endif
+
+        if (!Input.GetKey(KeyCode.LeftControl))
+        {
+            return;
+        }
+
+        if (__instance.Session == null)
+        {
+#if DEBUG
+            Logger.LogWarning("Session was null");
+#endif
+            return;
+        }
+
+        if (___inventoryController_0 == null)
+        {
+#if DEBUG
+            Logger.LogWarning("InventoryController was null");
+#endif
+            return;
+        }
+
+        if (__instance.Session.RagFair?.Available != true)
+        {
+#if DEBUG
+            Logger.LogWarning("Ragfair was not available");
+#endif
+            return;
+        }
+
+        if (___gclass3753_0 is not ContextInteractionsAbstractClass gclass)
+        {
+#if DEBUG
+            Logger.LogWarning($"Was not ContextInteractionsAbstractClass, was {___gclass3753_0.GetType().Name}");
+#endif
+            return;
+        }
+
+        if (!___gclass3753_0.AllInteractions.Contains(EItemInfoButton.AddOffer))
+        {
+#if DEBUG
+            Logger.LogWarning("Does not contain add to flea, skipping");
+#endif
+            return;
+        }
+
+        if (itemContext.ViewType != EItemViewType.Inventory)
+        {
+#if DEBUG
+            Logger.LogWarning($"Was not EItemViewType.Inventory, was {itemContext.ViewType}");
+#endif
+            return;
+        }
+
+        if (!itemContext.Item.CanSellOnRagfair)
+        {
+            return;
+        }
+
+        var parentItems = itemContext.Item.GetAllParentItems();
+        if (parentItems.Any(i => i is InventoryEquipment))
+        {
+            return;
+        }
+
+        if (itemContext.Item.IsNotEmpty())
+        {
+            return;
+        }
+
+        if (itemContext.Item.Parent.Container.ParentItem.TemplateId == "55d7217a4bdc2d86028b456d") // fix for UI Fixes
+        {
+            return;
+        }
+
+        var ragFair = __instance.Session.RagFair;
+        if (ragFair?.Disabled != false)
+        {
+            return;
+        }
+
+        if (ragFair.MyOffersCount == ragFair.MaxOffersCount)
+        {
+            return;
+        }
+
+        _postPriceData = new(ragFair, gclass,
+            __instance, ___gclass3753_0,
+            ___dictionary_0, ___inventoryController_0, itemContext,
+            __instance.HandbookClass);
+
+        ragFair.ISession.RagfairGetPrices(ReceivedPrices);
+    }
+
+    private static void ReceivedPrices(Result<Dictionary<string, float>> result)
+    {
+        _postPriceData.RagFair.method_35(result);
+        _postPriceData.RagFair.GetMarketPrices(_postPriceData.Item.TemplateId, SetPrices);
+    }
+
+    private static void HandlePostAddOffer()
+    {
+        foreach ((var item, var address) in _postPriceData.OfferDict)
+        {
+            address.RaiseRemoveEvent(item,
+                address.Equals(item.CurrentAddress) ? CommandStatus.Failed : CommandStatus.Succeed,
+                _postPriceData.InventoryController);
+        }
+
+        _postPriceData = default;
+        _canPost = true;
+    }
+
+    private static void SetPrices(ItemMarketPrices prices)
+    {
+#if DEBUG
+        Logger.LogWarning($"Average was {prices.avg} roubles");
+#endif
+        _postPriceData.AveragePrice = prices.avg;
+        if (_postPriceData.AveragePrice <= 0f)
+        {
+            return;
+        }
+
+        var averagePrice = _postPriceData.AveragePrice * _postPriceData.HandbookClass.StructuredItems[_roubleTpl].Data.Price;
+#if DEBUG
+        Logger.LogInfo($"Searching for posting price for {_postPriceData.Item.LocalizedShortName()}, with a stack amount of {_postPriceData.Item.StackObjectsCount}" +
+            $" and requirementsPrice of {averagePrice}");
+#endif
+        var postPrice = 0f;
+        if (CSF_Plugin.ShowListingPrice.Value)
+        {
+            postPrice = Mathf.CeilToInt(
+                (float)FleaTaxCalculatorAbstractClass.CalculateTaxPrice(_postPriceData.Item, _postPriceData.Item.StackObjectsCount,
+                averagePrice, false)
+            );
+        }
+        else
+        {
+            postPrice = averagePrice * _postPriceData.Item.StackObjectsCount;
+        }
+
+#if DEBUG
+        Logger.LogInfo($"Posting price was {postPrice} roubles");
+#endif
+
+        var dynamicInteractions = _postPriceData.InteractionsClass.Dictionary_0 ?? [];
+        dynamicInteractions[$"QUICK OFFER ({postPrice.FormatSeparate()} ₽)"] = new("QUICKOFFER", $"QUICK OFFER ({postPrice.FormatSeparate()} ₽)",
+            ClickQuickOffer, CacheResourcesPopAbstractClass.Pop<Sprite>("Characteristics/Icons/AddOffer"));
+
+        _postPriceData.ItemUiContext.ContextMenu.Show(_postPriceData.ItemUiContext.ContextMenu.transform.position,
+            _postPriceData.InteractionsClass, _postPriceData.ItemInfoDict, _postPriceData.Item);
+    }
+
+    private static void ClickQuickOffer()
+    {
+        _canPost = false;
+
+        _postPriceData.OfferDict.Add(_postPriceData.Item, _postPriceData.Item.Parent);
+        _postPriceData.Item.Parent.RaiseRemoveEvent(_postPriceData.Item, CommandStatus.Begin, _postPriceData.InventoryController);
+        Singleton<GUISounds>.Instance.PlayUISound(EUISoundType.TradeOperationComplete);
+        _postPriceData.RagFair.AddOffer(false, [_postPriceData.Item.Id], [
+                new()
+                {
+                    _tpl = "5449016a4bdc2d6f028b456f",
+                    count = _postPriceData.AveragePrice
+                }
+            ],
+            HandlePostAddOffer);
+    }
+}
