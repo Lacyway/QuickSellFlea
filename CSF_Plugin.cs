44cba1dfe73843f587a116505e0219d2b5448791
Lacyway <20912169+Lacyway@users.noreply.github.com>
2026-03-10 13:15:54 +0100

    Add keybind

--- CSF_Plugin.cs
+++ CSF_Plugin.cs
@@ -1,21 +1,27 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
+using Comfort.Common;
+using EFT.Communications;
 using EFT.InventoryLogic;
 using EFT.UI;
 using QuickSellFlea.Patches;
+using UnityEngine;
 
 namespace QuickSellFlea;
 
 [BepInPlugin("com.lacyway.csf", "QuickSellFlea", PluginVersion)]
 internal class CSF_Plugin : BaseUnityPlugin
 {
-    public const string PluginVersion = "1.1.3";
+    public const string PluginVersion = "1.2.0";
 
     internal static ManualLogSource CSF_Logger;
+    private static readonly EPostingCurrency[] _currencyValues =
+        (EPostingCurrency[])Enum.GetValues(typeof(EPostingCurrency));
 
     public static ConfigEntry<bool> ShowListingPrice { get; set; }
     public static ConfigEntry<EPostingCurrency> PostingCurrency { get; set; }
+    public static ConfigEntry<KeyboardShortcut> ChangeCurrencyKey { get; set; }
 
     protected void Awake()
     {
@@ -26,8 +32,42 @@ internal class CSF_Plugin : BaseUnityPlugin
             new ConfigDescription("Whether to show the listing price in the tooltip, otherwise the total sell value (of all items in the stack, if stackable)"));
         PostingCurrency = Config.Bind("QuickSellFlea", "Posting Currency", EPostingCurrency.RUB,
             new ConfigDescription("The currency to post the listings in"));
+        ChangeCurrencyKey = Config.Bind("QuickSellFlea", "Change Currency Key", new KeyboardShortcut(KeyCode.Insert),
+            new ConfigDescription("The key used to quickly change posting currency"));
 
         new ItemUiContext_GetItemContextInteractions_Patch().Enable();
+    }
+
+    protected void Update()
+    {
+        if (Input.GetKeyDown(ChangeCurrencyKey.Value.MainKey) && AreModifiersPressed(ChangeCurrencyKey.Value))
+        {
+            ChangeCurrency();
+        }
+    }
+
+    private void ChangeCurrency()
+    {
+        var currentIndex = Array.IndexOf(_currencyValues, PostingCurrency.Value);
+        PostingCurrency.Value = _currencyValues[(currentIndex + 1) % _currencyValues.Length];
+        if (Singleton<NotificationManagerClass>.Instantiated)
+        {
+            NotificationManagerClass.DisplayMessageNotification($"Currency set to {PostingCurrency.Value}",
+                iconType: ENotificationIconType.Note);
+        }
+    }
+
+    private static bool AreModifiersPressed(KeyboardShortcut shortcut)
+    {
+        foreach (var key in shortcut.Modifiers)
+        {
+            if (!Input.GetKey(key))
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
 
